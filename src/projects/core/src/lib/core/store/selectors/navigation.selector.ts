0e9b1dec65c985a4bf3bfc5d7c4a1def1d531843
Adrian Adamiak <a.adamiak@outlook.com>
2020-09-01 00:33:19 +0200

    ngrx router store selectors

--- src/projects/core/src/lib/core/store/selectors/navigation.selector.ts
+++ src/projects/core/src/lib/core/store/selectors/navigation.selector.ts
@@ -1,4 +1,5 @@
-import { createSelector } from '@ngrx/store';
+import * as fromRouter from '@ngrx/router-store';
+import { createSelector, createFeatureSelector } from '@ngrx/store';
 
 import { ICoreState, selectCore } from '../reducers';
 import { INavigationState } from '../reducers/navigation.reducer';
@@ -29,3 +30,16 @@ export const selectPreviousUrl = createSelector(
   selectNavigation,
   (state: INavigationState) => state && state.previousPath?.join('/'),
 );
+
+
+export const selectRouter = createFeatureSelector<any, fromRouter.RouterReducerState<any>>('router');
+
+export const {
+  selectCurrentRoute,   // select the current route
+  selectQueryParams,    // select the current route query params
+  selectQueryParam,     // factory function to select a query param
+  selectRouteParams,    // select the current route params
+  selectRouteParam,     // factory function to select a route param
+  selectRouteData,      // select the current route data
+  selectUrl,            // select the current url
+} = fromRouter.getSelectors(selectRouter);
